394d9267cf6c1e5d7341a9cd81e1f3ebc5d29a5c
DiegoGamesFight <35296262+diogoditorr@users.noreply.github.com>
2020-10-24 17:06:18 -0300

    Chapter 5

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -8,6 +8,23 @@
             <div class="user-profile__follower-count">
                 <strong>Followers: </strong> {{ followers }}
             </div>
+            <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
+                <label for="newTwoot"><strong>New Twoot</strong></label>
+                <textarea id="newTwoot" rows="4" v-model="newTwootContent"></textarea>
+
+                <div class="user-profile__create-twoot-type">
+                    <label for="newTwootType"><strong>Type: </strong></label>
+                    <select id="newTwootType" v-model="selectedTwootType">
+                        <option :value="option.value" v-for="(option, index) in twootTypes" :key="index">
+                            {{ option.name }}
+                        </option>
+                    </select>
+                </div>
+
+                <button>
+                    Twoot!
+                </button>
+            </form>
         </div>
 
         <div class="user-profile__twoots-wrapper">
@@ -32,17 +49,25 @@ export default {
 
     data() {
         return {
+            newTwootContent: '',
+            selectedTwootType: 'instant',
+            twootTypes: [
+                { value: 'draft', name: 'Draf' },
+                { value: 'instant', name: 'Instant Twoot' }
+            ],
             followers: 0,
             user: {
                 id: 1,
                 username: 'diogoditorr',
-                firstName: 'Diego',
-                lastName: 'Silva',
-                email: 'diegoditorr@hotmail.com',
+                firstName: 'Diogo',
+                lastName: 'Alvares',
+                email: 'diogoditorr@hotmail.com',
                 isAdmin: true,
                 twoots: [
                     { id: 1, content: 'Twotter is Amazing' },
-                    { id: 2, content: "Don't forget to subscriber to The Earth is Square!"}
+                    { id: 2, content: "Don't forget to subscriber to The Earth is Square!"},
+                    { id: 3, content: "Don't forget to subscriber to The Earth is Square!"},
+                    { id: 4, content: "Don't forget to subscriber to The Earth is Square!"}
                 ]
             }
         }
@@ -69,6 +94,16 @@ export default {
 
         toggleFavourite(id) {
             console.log(`Favourited Tweet #${id}`)
+        },
+
+        createNewTwoot() {
+            if (this.newTwootContent && this.selectedTwootType !== 'draft') {
+                this.user.twoots.unshift({
+                    id: this.user.twoots.length + 1,
+                    content: this.newTwootContent  
+                })
+                this.newTwootContent = '';
+            }
         }
     },
 
@@ -95,6 +130,7 @@ export default {
     background-color: white;
     border-radius: 5px;
     border: 1px solid #DFE3E8;
+    height: fit-content;
 }
 
 .user-profile__admin-badge {
@@ -106,7 +142,26 @@ export default {
     font-weight: bold;
 }
 
+.user-profile__username, 
+.user-profile__admin-badge,
+.user-profile__follower-count {
+    margin-bottom: 8px;
+}
+
 h1 {
     margin: 0;
+}
+
+.user-profile__twoots-wrapper {
+    display: grid;
+    grid-gap: 10px;
+}
+
+.user-profile__create-twoot {
+    border-top: 1px solid #DFE3E8;
+    padding-top: 20px;
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
 }
 </style>
