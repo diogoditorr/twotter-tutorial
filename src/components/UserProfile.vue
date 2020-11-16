095c4e54e15b3322c0450322ec500c71235be371
DiegoGamesFight <35296262+diogoditorr@users.noreply.github.com>
2020-11-16 20:02:12 -0300

    Chapter 7

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -2,12 +2,12 @@
     <div class="user-profile">
         <div class="user-profile__sidebar">
             <div class="user-profile__user-panel">
-                <h1 class="user-profile__username">@{{ user.username }}</h1>
-                <div class="user-profile__admin-badge" v-if="user.isAdmin">
+                <h1 class="user-profile__username">@{{ state.user.username }}</h1>
+                <div class="user-profile__admin-badge" v-if="state.user.isAdmin">
                     Admin
                 </div>
                 <div class="user-profile__follower-count">
-                    <strong>Followers: </strong> {{ followers }}
+                    <strong>Followers: </strong> {{ state.followers }}
                 </div> 
             </div>
             <CreateTwootPanel @add-twoot="addTwoot"/>
@@ -15,9 +15,9 @@
 
         <div class="user-profile__twoots-wrapper">
             <TwootItem 
-                v-for="twoot in user.twoots" 
+                v-for="twoot in state.user.twoots" 
                 :key="twoot.id" 
-                :username="user.username" 
+                :username="state.user.username" 
                 :twoot="twoot" 
             />
         </div>
@@ -25,6 +25,7 @@
 </template>
 
 <script>
+import { reactive } from 'vue';
 import TwootItem from "./TwootItem"
 import CreateTwootPanel from "./CreateTwootPanel"
 
@@ -32,9 +33,9 @@ export default {
     name: "UserProfile",
 
     components: { CreateTwootPanel, TwootItem },
-
-    data() {
-        return {
+    
+    setup() {
+        const state = reactive({
             followers: 0,
             user: {
                 id: 1,
@@ -50,6 +51,15 @@ export default {
                     { id: 4, content: "Don't forget to subscriber to The Earth is Square!"}
                 ]
             }
+        })
+
+        function addTwoot(twoot) {
+            state.user.twoots.unshift({ id: state.user.twoots.length + 1, content: twoot });
+        }
+
+        return {
+            state,
+            addTwoot
         }
     },
 
@@ -67,18 +77,16 @@ export default {
     //     }
     // },
 
-    methods: {
-        // followUser() {
-        //     this.followers++
-        // },
+    // methods: {
+    //     followUser() {
+    //         this.followers++
+    //     },
 
-        // toggleFavourite(id) {
-        //     console.log(`Favourited Tweet #${id}`)
-        // }
-        addTwoot(twoot) {
-            this.user.twoots.unshift({ id: this.user.twoots.length + 1, content: twoot });
-        }
-    },
+    //     toggleFavourite(id) {
+    //         console.log(`Favourited Tweet #${id}`)
+    //     }
+        
+    // },
 
     // Load when first load the component
     // mounted() {
